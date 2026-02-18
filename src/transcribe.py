2780ed69a4ae4d8d5bd0d82c09d8319893e09ca5
Liyovan <liyovanh@gmail.com>
2026-02-19 01:46:37 +0700

    Fixing Tranxcribe

--- src/transcribe.py
+++ src/transcribe.py
@@ -3,7 +3,9 @@ import json
 from pathlib import Path
 
 def Transribe(audio_path: Path, transicript_path: Path) ->str:
-    model= whisper.load_model('small')
-    result=model.transcribe(audio_path,transicript_path)
-    Path(transicript_path).write_text(json.dump(result,indent=2))
+    model= whisper.load_model('tiny')
+    result=model.transcribe(audio_path,word_timestamps=True)
+    Path(transicript_path).write_text(json.dumps(result,indent=2))
     return result
+
+Transribe("C:/Users/liyov/Desktop/MVP PROJECT/data/audio/extracted_audio.wav", "C:/Users/liyov/Desktop/MVP PROJECT/data/transcripts/transcript.json")
