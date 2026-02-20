1e42a76b9dab96d77ed5986b09765de09bc65674
Harpanada <liyovanh@gmail.com>
2026-02-20 22:28:39 +0700

    Build segmenter

--- src/extract_audio.py
+++ src/extract_audio.py
@@ -1,13 +1,24 @@
 import subprocess
 from pathlib import Path
+from transcribe import Transcribe
 
+def Extract(input_path) ->str:
+    # input_path = Path(input_path)
+    output_path="E:/MVP-AUTOCLIP-AI/data/audio/extracted_audio.wav"#Di deployment  harus diubah
 
-def Extract(input_path: Path) ->str:
-    input_path= Path(input_path)
-    output_path="C:/Users/liyov/Desktop/MVP PROJECT/data/audio/extracted_audio.wav"#Di deployment  harus diubah
-
-    subprocess.run(["ffmpeg","-i", str(input_path),
-                    "-vn", "-ac", "1" , 
-                    "-ar", "16000", str(output_path)])
+    subprocess.run([   
+        "ffmpeg",
+        "-i", input_path,
+        "-vn",           # No video
+        "-acodec", "pcm_s16le",  # WAV format
+        "-ar", "16000",  # Sample rate 16kHz (required by Whisper)
+        "-ac", "1",     # Mono
+        output_path,
+        "-y"])
+    return output_path
     
 
+
+extracted="E:\MVP-AUTOCLIP-AI\data\input\Borong Bitcoin Episode 33 - Februari 2026 - Timothy Ronald (1080p, h264).mp4"
+extracted=Extract(extracted)
+Transcribe(extracted,"E:/MVP-AUTOCLIP-AI/data/transcripts/tarnscript.json")
--- src/transcribe.py
+++ src/transcribe.py
@@ -2,10 +2,8 @@ import whisper
 import json
 from pathlib import Path
 
-def Transribe(audio_path: Path, transicript_path: Path) ->str:
+def Transcribe(audio_path: Path, transicript_path: Path) ->str:
     model= whisper.load_model('tiny')
     result=model.transcribe(audio_path,word_timestamps=True)
     Path(transicript_path).write_text(json.dumps(result,indent=2))
     return result
-
-Transribe("C:/Users/liyov/Desktop/MVP PROJECT/data/audio/extracted_audio.wav", "C:/Users/liyov/Desktop/MVP PROJECT/data/transcripts/transcript.json")
